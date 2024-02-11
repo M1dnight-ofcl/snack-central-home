9dad9ccc8b6ca400c2f4df35235bcfad43982b96
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-02-11 18:12:51 -0500

    menu template

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { render } from 'preact'
+// Pages ----------------
 import App from './app.jsx'
 import ContactUs from './contact.jsx'
+import Menu from './menu.jsx'
+// ----------------------
 import Router from 'preact-router';
 import { Link } from 'preact-router/match';
 import { h } from 'preact';
@@ -12,10 +15,12 @@ const Home=()=>{
         <div id="logo"></div>
         <Link className="navlink" activeClassName="active" href="/">Home</Link>
         <Link className="navlink" activeClassName="active" href="/contactus">Contact Us</Link>
+        <Link className="navlink" activeClassName="active" href="/menu">Menu</Link>
     </nav>
     <Router>
         <App path="/" />
         <ContactUs path="/contactus" />
+        <Menu path="/menu" />
     </Router>
     </>)
 }
---
+++ src/menu.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'preact/hooks'
+import "./style/menu.css"
+
+const Menu=()=>{
+    return(<>
+        <div id="c_header">
+            <h1>Menu</h1>
+        </div>
+        
+    </>)
+}
+export default Menu;
