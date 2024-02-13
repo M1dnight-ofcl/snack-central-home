83d93f4a562f8209a6bd7cbe0fb82e322922437d
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2024-02-13 15:16:22 -0500

    added contact email support :>

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,13 +1,20 @@
-import { useState, useEffect } from 'preact/hooks';
+import { useState, useEffect, useRef } from 'preact/hooks';
+import emailjs from '@emailjs/browser';
+import { SHA256, enc } from 'crypto-js';
 import "./style/contact.css";
 const ContactUs=()=>{
     let allInputs=[];
+    const form=useRef();
     const Input=(prop)=>{
         allInputs.push(prop.id);
         return(<>
-            <label for={prop.id}>{prop.title}</label><br/>
-            <input name={prop.id} id={prop.id} type="text" />
+            <label for={prop.name}>{prop.title}</label><br/>
+            <input name={prop.name} id={prop.id} type="text" />
         </>);
+    }
+    const GenerateContactFormKey=()=>{
+        return SHA256(btoa(Math.random() + 1).toString(36).substring(7))
+        .toString(enc.Hex).split('',6).join('');
     }
     const Click=(e)=>{
         e.preventDefault();
@@ -17,8 +24,24 @@ const ContactUs=()=>{
             msg: document.getElementById('cf_msg').value,
         }
         if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === "")
-            alert(`localhost detected\nDebug stats:\nname: "${cf_dat.name}"\nemail: "${cf_dat.email}"\nmsg: "${cf_dat.msg}"`);
-        else alert('Coming Soon! (sorry)');
+            console.log(`localhost detected\nDebug stats:\nname: "${cf_dat.name}"\nemail: "${cf_dat.email}"\nmsg: "${cf_dat.msg}"`);
+        // else alert('Coming Soon! (sorry)');
+        emailjs.sendForm(
+            'snaclcentralemailjs', 
+            'contactus_sent', 
+            form.current, {publicKey: 'i59EM-dm7QDrNOM0z',})
+        .then(
+            () => { console.log('SUCCESS!'); },
+            (error) => { console.log('FAILED...', error.text); },
+        );
+        emailjs.sendForm(
+            'snaclcentralemailjs', 
+            'contactus_toadmin', 
+            form.current, {publicKey: 'i59EM-dm7QDrNOM0z',})
+        .then(
+            () => { console.log('SUCCESS!'); },
+            (error) => { console.log('FAILED...', error.text); },
+        );
     }
     return(<>
         <div id="c_header">
@@ -27,10 +50,13 @@ const ContactUs=()=>{
         <section className='c_section'>
             <div className='contentWrapper'>
                 <div className='form'>
-                    <Input title="Name" id="cf_name" />
-                    <Input title="Email" id="cf_email" />
-                    <Input title="Message" id="cf_msg" />
-                    <button id="c_submit" onClick={(e)=>Click(e)}>Submit</button>
+                    <form className='c_form' ref={form} onSubmit={(e)=>Click(e)}>
+                        <Input title="Name" id="cf_name"  name="user_name"/>
+                        <Input title="Email" id="cf_email" name="user_email" />
+                        <Input title="Message" id="cf_msg" name="message" />
+                        <input type='text' value={GenerateContactFormKey()} name="cf_id" id="cf_id" style="display:none;" />
+                        <input type="submit" id="c_submit" value="Submit" />
+                    </form>
                 </div>
             </div>
         </section>
